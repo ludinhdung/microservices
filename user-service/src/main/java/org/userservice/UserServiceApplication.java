8126036ddfb496a93c9212718d638e903d05299e
Dinh Dung <dungdung23092002@gmail.com>
2024-05-26 11:57:04 +0700

    change config database

--- user-service/src/main/java/org/userservice/UserServiceApplication.java
+++ user-service/src/main/java/org/userservice/UserServiceApplication.java
@@ -1,8 +1,14 @@
 package org.userservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.userservice.entity.User;
+import org.userservice.repository.UserRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +16,18 @@ public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initData(UserRepository userRepository) {
+        return args -> {
+            List<User> users = List.of(
+                    new User("John Doe", "email@gmail.com"),
+                    new User("Jane Doe", "email@gmail.com")
+            );
+
+            userRepository.saveAll(users);
+        };
     }
 
 }
